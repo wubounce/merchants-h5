d42a523758f5a959d34161bca73da4b06626ad97
syy <ykmodel@126.com>
2018-07-24 15:34:21 +0800

    店铺所有前端部分完成，除了接口相关，地图调用正在开发中

--- src/page/shop/shopList.vue
+++ src/page/shop/shopList.vue
@@ -3,14 +3,33 @@
 	  <q-header :title="title"></q-header>
     <ul>
       <li v-for="(item,index) in list" :key="index" @click="toShopDetail">
-        <p class="top"><span class="iconMark discount">惠</span><span class="iconMark reserve">预</span><span class="shopName">{{item.shopName}}</span></p>
+        <p class="top">
+          <!-- <span class="shopName">{{item.shopName}}</span>
+          <span>
+            <span class="iconMark discount">惠</span><span class="iconMark reserve">预</span>
+          </span> -->
+          <span>{{item.shopName}}</span>
+          <span><span class="iconMark discount">惠</span><span class="iconMark reserve">预</span></span>
+        </p>
         <div class="bottom">
-          <p>
+          <div class="kindof">
+            <div class="text">分类</div>
+            <div class="text-value">{{item.shopTypeName}}</div>
+          </div>
+          <div class="kindof">
+            <div class="text">设备</div>
+            <div class="text-value">{{item.machineCount}}<span class="little-font">台</span></div>
+          </div>
+          <div class="kindof">
+            <div class="text">收益</div>
+            <div class="text-value">{{item.orderPrice}}<span class="little-font">元</span></div>
+          </div>
+          <!-- <p>
             <span class="kindof">分类</span><span class="size">{{item.shopTypeName}}</span>
             <span class="kindof">设备</span><span class="size">{{item.machineCount}}台</span>
             <br>
             <span class="kindof">收益</span><span class="size">{{item.orderPrice}}元</span>
-          </p>
+          </p> -->
         </div>
       </li>
     </ul>
@@ -26,9 +45,9 @@ export default {
        title:'店铺管理',
        list:[{
           shopId:'12345dsuisydwedbewi1823',
-          machineCount:50,
+          machineCount:54444440,
           shopName:'常熟高新园中等专业学校',
-          orderPrice:10000.00,
+          orderPrice:'999999100.00',
           shopTypeName:'流动人口社区',
           ifAddTime:'1',
           isReserve:'0'
@@ -37,7 +56,7 @@ export default {
           shopId:'12345dsuisydwedbewi1823',
           machineCount:50,
           shopName:'常熟高新园中等专业学校',
-          orderPrice:10000.00,
+          orderPrice:'10000.00',
           shopTypeName:'流动人口社区',
           ifAddTime:'1',
           isReserve:'0'
@@ -46,7 +65,7 @@ export default {
           shopId:'12345dsuisydwedbewi1823',
           machineCount:50,
           shopName:'常熟高新园中等专业学校',
-          orderPrice:10000.00,
+          orderPrice:'10000.00',
           shopTypeName:'流动人口社区',
           ifAddTime:'1',
           isReserve:'0'
@@ -55,7 +74,7 @@ export default {
           shopId:'12345dsuisydwedbewi1823',
           machineCount:50,
           shopName:'常熟高新园中等专业学校',
-          orderPrice:10000.00,
+          orderPrice:'10000.00',
           shopTypeName:'流动人口社区',
           ifAddTime:'1',
           isReserve:'0'
@@ -64,7 +83,7 @@ export default {
           shopId:'12345dsuisydwedbewi1823',
           machineCount:50,
           shopName:'常熟高新园中等专业学校',
-          orderPrice:10000.00,
+          orderPrice:'10000.00',
           shopTypeName:'流动人口社区',
           ifAddTime:'1',
           isReserve:'0'
@@ -76,12 +95,12 @@ export default {
     toShopDetail() {
       this.$router.push({
         name:'shopDetail'
-      })
+      });
     },
     toAddShop() {
       this.$router.push({
         name:'addShop'
-      })
+      });
     }
   },
   components: {
@@ -99,6 +118,12 @@ section {
       background:rgba(255,255,255,1);
       margin-bottom:0.23rem;
       .top {
+        display:flex;
+        justify-content: space-between;
+        border-bottom: 1px solid #F8F8F8;
+        background-color: #fff; 
+        font-size: 0.45rem;
+        padding: 0.3rem; 
         .iconMark {
           font-size: 0.3rem;
           color: #fff;
@@ -123,27 +148,48 @@ section {
           font-family:PingFangSC-Medium;
           color:rgba(51,51,51,1);
           line-height:0.45rem;
-          font-weight: bold;
         }
       }
       .bottom {
         width: 92%;
         height:1.96rem;
-        background:rgba(248,252,255,1);
         border-radius:0.1rem;
         margin: 0.2rem auto;
+        display: flex;
+        justify-content: center;
         .kindof {
-          background: #e0f2ff;
-          border-radius: 0.09rem;
-          font-size: 0.3rem;
-          color: rgba(24,144,255,1);
-          padding: 0.12rem 0.22rem;
-          margin-left:0.4rem;
+          width: 35%;
+          text-align: center;
+          &:nth-child(2) {
+            border-left: 1px solid #DFE6FF;
+            border-right: 1px solid #DFE6FF;
+          }
+          .text {
+            margin-top: 0.4rem;
+            color: #999999;
+            font-size: 0.2rem;
+          }
+          .text-value {
+            color:#1890FF;
+            font-size: 0.4rem;
+            margin-top: 0.2rem;
+            .little-font {
+              font-size: 0.01rem;
+            }
+          }
         }
-        .size {
-          font-size: 0.35rem;
-          margin-left: 0.2rem;
-        }
+        // .kindof {
+        //   background: #e0f2ff;
+        //   border-radius: 0.09rem;
+        //   font-size: 0.3rem;
+        //   color: rgba(24,144,255,1);
+        //   padding: 0.12rem 0.22rem;
+        //   margin-left:0.4rem;
+        // }
+        // .size {
+        //   font-size: 0.35rem;
+        //   margin-left: 0.2rem;
+        // }
       }
     }
   }
@@ -161,7 +207,7 @@ section {
     }
     .iconShop {
       font-size: 0.35rem;
-      margin-top: -0.05rem;
+      margin-top: -0.2rem;
     }
   }
 }
