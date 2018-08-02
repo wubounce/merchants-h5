d1cef76cd8da99ec9cc0adce8327ec784718b78e
吴晓红 <18311017120@163.com>
2018-08-02 17:57:00 +0800

    人员管理

--- src/page/member/index.vue
+++ src/page/member/index.vue
@@ -2,20 +2,27 @@
 <div class="member" v-title="title">
   <div class="permissions" v-if="$store.getters.has('mer:person:list')">暂无相关页面权限</div>
   <div v-else>
+
     <div class="no-discount-list" v-if="list.length<=0">暂无二级管理账号</div>
-    <div class="meber-list" v-for="(item,index) in list" :key="index">
-      <div class="momber-wrap">
-        <div class="name">
-          <router-link :to="{name:'detailMember',query:{ id:item.id }}">
-            <p>{{item.userName}}</p><p class="phonenum">{{item.phone}}</p>
-          </router-link>
+    <div class="page-loadmore-wrapper" ref="wrapper" :style="{ height: wrapperHeight + 'px' }">
+     <mt-loadmore :top-method="loadTop" :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" :auto-fill="false" ref="loadmore">
+        <div class="meber-list" v-for="(item,index) in list" :key="index">
+          <div class="momber-wrap">
+            <div class="name">
+              <router-link :to="{name:'detailMember',query:{ id:item.id }}">
+                <p>{{item.userName}}</p><p class="phonenum">{{item.phone}}</p>
+              </router-link>
+            </div>
+            <div class="phone">
+              <div class="right"><mt-switch v-model="item.isLock" class="check-switch" @change="lockOperator(item.id,item.isLock)"></mt-switch></div>
+            </div>
+          </div>
+          <p class="memberdesc">权限：<span v-for="(items,index) in item.list" :key="index">{{items.name}}<i v-if="index !== (item.list.length-1)">,</i></span></p>
         </div>
-        <div class="phone">
-          <div class="right"><mt-switch v-model="item.isLock" class="check-switch" @change="lockOperator(item.id,item.isLock)"></mt-switch></div>
-        </div>
-      </div>
-      <p class="memberdesc">权限：<span v-for="(items,index) in item.list" :key="index">{{items.name}}<i v-if="index !== (item.list.length-1)">,</i></span></p>
+        <div v-if="allLoaded" class="nomore-data">没有更多了</div>
+      </mt-loadmore>
     </div>
+
     <div class="addmember" @click="addmemeber" v-has="'mer:person:add'">
       <span class="order-action iconfont icon-tianjia"></span><br>
       <span>人员</span>
@@ -33,32 +40,56 @@ export default {
     return {
       title: '人员管理',
       value: false,
-      list:[]
+      list:[],
+      page: 1,//页码
+      pageSize:10,
+      total:null,
+      allLoaded: false,//数据是否加载完毕
+      wrapperHeight: 0,//容器高度
     };
   },
   mounted() {
-    
+    let windowWidth = document.documentElement.clientWidth;//获取屏幕高度
+    this.wrapperHeight = document.documentElement.clientHeight - 30;
   },
   created(){
     this.getMemberList();
   },
   methods: {
     async getMemberList(){
-      let res = await operatorListFun();
+      let payload = {page:this.page,pageSize:this.pageSize};
+      let res = await operatorListFun(qs.stringify(payload));
       if (res.code === 0) {
-        res.data = res.data ? res.data :[];
-        res.data.forEach((item)=>{
+        res.data.items = res.data ? res.data.items :[];
+        res.data.items.forEach((item)=>{
           if (item.isLock === 0) {
             item.isLock = true;
           } else {
             item.isLock = false;
           }
         });
-        this.list = res.data;
+        this.list = [...this.list,...res.data.items];
       }else {
         this.$toast(res.msg);
       }
       
+    },
+    loadBottom() {
+      this.page += 1;
+      let allpage = this.total/this.pageSize;
+      if(this.page <= allpage){
+        this.getMemberList();
+      }else{
+        this.allLoaded = true;//模拟数据加载完毕 禁用上拉加载
+      }
+      this.$refs.loadmore.onBottomLoaded();
+    },
+    loadTop() {
+      this.page = 1;
+      this.list = [];
+      this.getMemberList();
+      this.allLoaded = false;//下拉刷新时解除上拉加载的禁用
+      this.$refs.loadmore.onTopLoaded();
     },
     async lockOperator(id,isLock){
       if (isLock === true) {
@@ -150,6 +181,15 @@ export default {
     padding-top: 4rem;
     color: #999;
   }
+  .page-loadmore-wrapper {
+    overflow: scroll
+  }
+  .nomore-data {
+    text-align: center;
+    color: #999;
+    font-size: 12px;
+    margin-bottom: 0.266667rem;
+  }
 </style>
 <style>
   .member .mint-header {
@@ -159,5 +199,14 @@ export default {
   .member .mint-switch-input:checked + .mint-switch-core {
     border-color: #4DD865;
     background-color: #4DD865;
+  }
+  .member .mint-header {
+    background: #fff !important;
+    border-bottom:1px solid #DCE0E6;
+  }
+  .member .mint-loadmore-top, .mint-loadmore-text {
+    font-size: 14px;
+    text-align: center;
+    color: #999;
   }
 </style>
