7110610695d0aa512cdeee499c0e203b84807ac8
ljb <654146014@qq.com>
2018-08-16 04:21:27 +0800

    bug

--- src/utils/Web.js
+++ src/utils/Web.js
@@ -47,18 +47,19 @@ const Web = {
       url: this.getSignUrl()
     };
     getWxconfigFun(qs.stringify(payload)).then(config => {
+      console.log(config)
       wx.config({
         debug: process.env.NODE_ENV === 'development', // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        appId: config.appId, // 必填，公众号的唯一标识
-        timestamp: config.timeStamp, // 必填，生成签名的时间戳
-        nonceStr: config.nonceStr, // 必填，生成签名的随机串
-        signature: config.sign, // 必填，签名
+        appId: config.data.appId, // 必填，公众号的唯一标识
+        timestamp: config.data.timestamp, // 必填，生成签名的时间戳
+        nonceStr: config.data.nonceStr, // 必填，生成签名的随机串
+        signature: config.data.signature, // 必填，签名
         jsApiList: ['chooseWXPay', 'scanQRCode', 'checkJsApi', 'getLocation'] // 必填，需要使用的JS接口列表
       })
       wx.error(err => {
-				window.location.reload() 
-				// alert(url)
-				// alert(JSON.stringify(err))
+				 window.location.reload() 
+        // alert(url)
+				 alert(JSON.stringify(err))
 			})
     })
   },
