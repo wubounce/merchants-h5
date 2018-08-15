9a903d832ed2579788efef8adc017afb82ed8306
gaoqing <gq@qiekj.com>
2018-08-16 06:54:25 +0800

    wechant sign error

--- src/utils/Web.js
+++ src/utils/Web.js
@@ -5,6 +5,7 @@ import Api from '@/utils/Api';
 import store from '@/store';
 import axios from 'axios';
 import { getWxconfigFun } from '@/service/device';
+import { getToken, removeMenu } from '@/utils/tool';
 
 // 页面相关工具类
 const Web = {
@@ -46,18 +47,20 @@ const Web = {
     let payload = {
       url: this.getSignUrl()
     };
+    
     getWxconfigFun(qs.stringify(payload)).then(config => {
       console.log(config)
       wx.config({
         debug: process.env.NODE_ENV === 'development', // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: config.data.appId, // 必填，公众号的唯一标识
-        timestamp: config.data.timestamp, // 必填，生成签名的时间戳
+        timestamp: config.data.timeStamp, // 必填，生成签名的时间戳
         nonceStr: config.data.nonceStr, // 必填，生成签名的随机串
-        signature: config.data.signature, // 必填，签名
+        signature: config.data.sign, // 必填，签名
         jsApiList: ['chooseWXPay', 'scanQRCode', 'checkJsApi', 'getLocation'] // 必填，需要使用的JS接口列表
       })
       wx.error(err => {
-				 window.location.reload() 
+        // alert(payload.url)
+				 // window.location.reload() 
         // alert(url)
 				 alert(JSON.stringify(err))
 			})
