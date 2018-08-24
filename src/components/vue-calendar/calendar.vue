3e245c01b6c1c4bdd0b28a7bf4ea89b759287031
吴晓红 <18311017120@163.com>
2018-08-24 15:58:09 +0800

    报表日期插件

--- src/components/vue-calendar/calendar.vue
+++ src/components/vue-calendar/calendar.vue
@@ -314,15 +314,21 @@ export default {
         // 自定义星期名称
         weeks: {
             type: Array,
+            // default:function(){
+            //     return window.navigator.language.toLowerCase() == "zh-cn"?['日', '一', '二', '三', '四', '五', '六']:['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+            // }
             default:function(){
-                return window.navigator.language.toLowerCase() == "zh-cn"?['日', '一', '二', '三', '四', '五', '六']:['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+                return ['日', '一', '二', '三', '四', '五', '六'];
             }
         },
         // 自定义月份
         months:{
             type: Array,
+            // default:function(){
+            //     return window.navigator.language.toLowerCase() == "zh-cn"?['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']:['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            // }
             default:function(){
-                return window.navigator.language.toLowerCase() == "zh-cn"?['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']:['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                return ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
             }
         },  
         
