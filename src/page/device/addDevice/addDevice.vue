d81e24c43ce0b533e0533ed072afe93a0962a4bc
ljb <654146014@qq.com>
2018-09-04 16:08:22 +0800

    新增设备一级类型接口替换

--- src/page/device/addDevice/addDevice.vue
+++ src/page/device/addDevice/addDevice.vue
@@ -119,7 +119,7 @@
   import { MessageBox } from 'mint-ui';
   import Button from "@/components/Button/Button";
   import selectpickr from '@/components/selectPicker';
-  import { getWxconfigFun,getShopFun,getShopListParentTypeFun,listSubTypeByFun,deviceAddorEditFun,getFunctionSetListFun } from '@/service/device';
+  import { getWxconfigFun,getShopFun,getListParentTypeFun,listSubTypeByFun,deviceAddorEditFun,getFunctionSetListFun } from '@/service/device';
  
   export default {
     data() {
@@ -385,7 +385,7 @@
       async checkFirstClass() { //获取一级列表
         if(this.fromdata.shopType.id){
           let payload = {shopId:this.fromdata.shopType.id};
-          let res = await getShopListParentTypeFun(payload);
+          let res = await getListParentTypeFun(payload);
           this.slotsFirst[0].values = res;
           this.parentType = true;        
         }else{
