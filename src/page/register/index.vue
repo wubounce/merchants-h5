3c20caa749fe7c87e55677c0d247610fe2cb973d
ljb <654146014@qq.com>
2018-09-21 13:49:34 +0800

    账号登陆注册

--- src/page/register/index.vue
+++ src/page/register/index.vue
@@ -2,10 +2,16 @@
   <section class="register">
     <div ref="registerForm" :model="register" class="register-form">
     <div class="form-group">
-        <input type="number" placeholder="请输入手机号" v-model="register.phone" @input="disabledBtn" required>
+      <input type="number" placeholder="请输入手机号" v-model="register.phone" @input="disabledBtn" required>
     </div>
     <div class="form-group">
-        <input type="text" placeholder="请输入姓名" v-model="register.name" @input="disabledBtn" required>
+      <div> 
+        <input type="text" placeholder="请输入验证码" v-model="register.vcode" @input="disabledBtn" required>
+      </div>
+      <div class="verificode">
+        <p class="countdown" v-if="!btn">{{time}}s后重新获取</p>
+        <p class="sendcode" @click="sendcode" v-if="btn">获取验证码</p>
+      </div>
     </div>
     <div class="form-group">
         <input type="password" placeholder="请输入密码（6-20位）" v-model="register.code" @input="disabledBtn" required>
@@ -21,84 +27,105 @@
 </template>
 
 <script>
+  import { smscodeFun, validateCodeFun} from '@/service/resetPwd';
   import { validatPhone , validatName , validatPwd } from '@/utils/validate';
   export default{
     data() {
       return {
         register: {
           phone: '',
-          name: '',
+          vcode: '',
           code: '',
           sureCode: ''
         },
-        disabled:true
+        disabled:true,
+        timer: null,
+        time: 0,
+        btn: true
       };
     },
     methods: {
-        disabledBtn() {  //输入框未填完，按钮不可点击
-          if(!this.register.phone || !this.register.name || !this.register.code || !this.register.sureCode) {
-            this.disabled = true;
-          }else {
-            this.disabled = false;
-          }
-        },
-        validatePhone() { //验证手机
-          if (this.register.phone === '') {
-            this.$toast({message: "请输入手机号码" });
-            return false;
-          }else if(!validatPhone(this.register.phone)) {
-            this.$toast({message: "请输入正确的手机号码" });
-            return false;
-          } 
-          return true;
-        }, 
-        validateName() {  //验证用户名
-          if (this.register.name === '') {
-            this.$toast({message: "请输入用户名"});
-            return false;
-          }else if(!validatName(this.register.name)) {
-            this.$toast({message: "用户名2-20个字符，支持中文和英文" });
-            return false;
-          }
-          return true;
-        },
-        validateCode() {  //验证密码
-          if (this.register.code === '') {
-            this.$toast({message: "请输入密码"});
-            return false;
-          }else if(!validatPwd(this.register.code)) {
-            this.$toast({message: "密码6-20个字符，只支持数字和英文" });
-            return false;
-          }
-          return true;
-        },
-
-        validateSureCode() { //验证确认密码
-          if (this.register.sureCode === '') {
-            this.$toast({message: "请输入密码"});
-            return false;
-          }else if (!validatPwd(this.register.sureCode)) {
-            this.$toast({message: "密码6-20个字符，只支持数字和英文" });
-            return false;
-          }else if (this.register.code !== this.register.sureCode){
-            this.$toast({message: "两次输入的密码不一致"});
-            return false;
-          }
-          return true;
-        },
-
-        goToNext() {
-          if(!this.validatePhone()) return false;
-          if(!this.validateName()) return false;
-          if(!this.validateCode()) return false;
-          if(!this.validateSureCode()) return false;
-          sessionStorage.setItem('registerPhone',this.register.phone);
-          sessionStorage.setItem('registerName',this.register.name);
-          this.$router.push({
-            name: "referee",
-            query: ({phone:this.register.phone, name:this.register.name, password:this.register.code})
-          });
+      async sendcode() {
+        if (this.validatePhone()) {
+          let res = await smscodeFun({phone:this.register.phone,mark:true});
+              this.time = 60;
+              this.btn = false;
+              this.countdown();
         }
+      },
+      countdown() {
+        this.btn = false;
+        this.timer = setInterval(() => {
+          if (--this.time <= 0) {
+            clearInterval(this.timer);
+            this.btn = true;
+          }
+        }, 1000);
+      },
+      disabledBtn() {  //输入框未填完，按钮不可点击
+        if(!this.register.phone || !this.register.vcode || !this.register.code || !this.register.sureCode) {
+          this.disabled = true;
+        }else {
+          this.disabled = false;
+        }
+      },
+      validatePhone() { //验证手机
+        if (this.register.phone === '') {
+          this.$toast({message: "请输入手机号码" });
+          return false;
+        }else if(!validatPhone(this.register.phone)) {
+          this.$toast({message: "请输入正确的手机号码" });
+          return false;
+        } 
+        return true;
+      }, 
+      validateName() {  //验证用户名
+        if (this.register.name === '') {
+          this.$toast({message: "请输入用户名"});
+          return false;
+        }else if(!validatName(this.register.name)) {
+          this.$toast({message: "用户名2-20个字符，支持中文和英文" });
+          return false;
+        }
+        return true;
+      },
+      validateCode() {  //验证密码
+        if (this.register.code === '') {
+          this.$toast({message: "请输入密码"});
+          return false;
+        }else if(!validatPwd(this.register.code)) {
+          this.$toast({message: "密码6-20个字符，只支持数字和英文" });
+          return false;
+        }
+        return true;
+      },
+
+      validateSureCode() { //验证确认密码
+        if (this.register.sureCode === '') {
+          this.$toast({message: "请输入密码"});
+          return false;
+        }else if (!validatPwd(this.register.sureCode)) {
+          this.$toast({message: "密码6-20个字符，只支持数字和英文" });
+          return false;
+        }else if (this.register.code !== this.register.sureCode){
+          this.$toast({message: "两次输入的密码不一致"});
+          return false;
+        }
+        return true;
+      },
+
+      goToNext() {
+        if(!this.validatePhone()) return false;
+        if(!this.validateName()) return false;
+        if(!this.validateCode()) return false;
+        if(!this.validateSureCode()) return false;
+        sessionStorage.setItem('registerPhone',this.register.phone);
+        sessionStorage.setItem('registerName',this.register.name);
+        this.$router.push({
+          name: "referee",
+          query: ({phone:this.register.phone, name:this.register.name, password:this.register.code})
+        });
+      }
     },
       mounted() {
       if(sessionStorage.getItem('registerPhone')) this.register.phone = sessionStorage.getItem('registerPhone');  
@@ -115,9 +142,27 @@
       padding: 0 0.93rem;
       .form-group {
         height: 1.57rem;
-        line-height: 1.57rem;
         font-size: 0.43rem;
         box-sizing: border-box;
+        position: relative;
+        .verificode {
+          position: absolute;
+          right: 0rem;
+          top: 0.46rem;
+          p {
+            font-size: 12px;
+            border: 1px solid #1890FF;
+            padding: 0.15rem;
+            border-radius: 5px
+          }
+          .countdown {
+            border: 1px solid #999;
+            color: #999;
+          }
+          .sendcode {
+            color: #1890FF;
+          }
+        }
         input {
           height: 1.57rem;
           border-bottom: 0.01rem solid #e5e5e5;
