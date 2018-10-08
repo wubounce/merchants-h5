33b53fa4050437ffe33d2ff85a42773a24f99ce3
lujiangbo <15868854512@163.com>
2018-10-08 15:53:21 +0800

    bug

--- src/utils/Web.js
+++ src/utils/Web.js
@@ -51,7 +51,7 @@ const Web = {
     getWxconfigFun(payload).then(config => {
       // console.log(config)
       wx.config({
-        debug: process.env.NODE_ENV === 'development', // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        //debug: process.env.NODE_ENV === 'development', // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: config.appId, // 必填，公众号的唯一标识
         timestamp: config.timeStamp, // 必填，生成签名的时间戳
         nonceStr: config.nonceStr, // 必填，生成签名的随机串
