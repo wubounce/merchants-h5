24cacb2dcca48bab5b87742b4bb7344b74ec6b98
wubounce <18311017120@163.com>
2018-12-02 15:14:49 +0800

    客服电话bug

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_URL: '"http://192.168.5.90:8089/"'
+  // API_URL: '"https://pre-bmapi.qiekj.com/"'
   // API_URL: '"http://192.168.4.206:8089/"'
 })
