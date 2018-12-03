33fed59bdcdf0f1fe1077c35b0665b036bc81a32
吴晓红 <18311017120@163.com>
2018-12-03 18:40:18 +0800

    水位

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://192.168.5.90:8089/"'
+  API_URL: '"http://192.168.5.60:8089/"'
   // API_URL: '"http://192.168.4.206:8089/"'
 })
